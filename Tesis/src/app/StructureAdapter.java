634f4a614a6dfedf005749f52ebbcef5f0d12893
Diana Marcela <dm.gutierrez11@uniandes.edu.co>
2020-05-04 13:24:55 -0500

    add edge

--- Tesis/src/app/StructureAdapter.java
+++ Tesis/src/app/StructureAdapter.java
@@ -29,7 +29,7 @@ import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.util.mxConstants;
 
-import student.tree.*;
+import estudiante.*;
 
 
 
@@ -59,6 +59,8 @@ public class StructureAdapter extends JApplet
 	@SuppressWarnings({ })
 	private StandardMethods sm = new StandardMethods();
 	private JFrame frame;
+	private JPanel infoPanel;
+	private int structureType = sm.structureType();
 
 	/**
 	 * An alternative starting point for this demo, to also allow running this applet as an
@@ -84,6 +86,7 @@ public class StructureAdapter extends JApplet
 		options.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 20));
 
 
+		// Search for nodes panel
 		JPanel p1 = new JPanel();
 		p1.setLayout(new BoxLayout(p1, BoxLayout.PAGE_AXIS));		
 		p1.add(new JLabel("Buscar nodos"));
@@ -95,6 +98,16 @@ public class StructureAdapter extends JApplet
 		p1.add(getNodeButton);
 		options.add(p1);
 
+		ActionListener searchNode = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String node = tf1.getText();
+				getNodeSet(node);
+			}
+		};
+		getNodeButton.addActionListener(searchNode);
+
+		//Search for neighbors panel
 		JPanel p2 = new JPanel();
 		p2.setLayout(new BoxLayout(p2, BoxLayout.PAGE_AXIS));		
 		p2.add(new JLabel("Buscar vecinos"));
@@ -106,6 +119,16 @@ public class StructureAdapter extends JApplet
 		p2.add(getNeighbors);
 		options.add(p2);
 
+		ActionListener searchNeighbors = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String node = tf2.getText();
+				getNeighbors(node);
+			}
+		};
+		getNeighbors.addActionListener(searchNeighbors);
+
+		//Add a node panel
 		JPanel p3 = new JPanel();
 		p3.setLayout(new BoxLayout(p3, BoxLayout.PAGE_AXIS));		
 		p3.add(new JLabel("Agregar nodo"));
@@ -117,56 +140,6 @@ public class StructureAdapter extends JApplet
 		p3.add(addNode);
 		options.add(p3);
 
-		JPanel p5 = new JPanel();
-		p5.setLayout(new BoxLayout(p5, BoxLayout.PAGE_AXIS));		
-		p5.add(new JLabel("Eliminar nodo"));
-		JTextField tf6 = new JTextField(15);
-		tf6.setPreferredSize(new Dimension(getWidth(),20));
-		p5.add(tf6);
-		JButton delNode = new JButton("Eliminar");
-		delNode.setAlignmentX(LEFT_ALIGNMENT);
-		p5.add(delNode);
-		options.add(p5);
-
-		JPanel p4 = new JPanel();
-		p4.setLayout(new BoxLayout(p4, BoxLayout.PAGE_AXIS));		
-		p4.add(new JLabel("Buscar arcos"));
-		JTextField tf5 = new JTextField(15);
-		tf5.setPreferredSize(new Dimension(getWidth(),20));
-		p4.add(tf5);
-		JButton getEdges = new JButton("Buscar");
-		getEdges.setAlignmentX(LEFT_ALIGNMENT);
-		p4.add(getEdges);
-		options.add(p4);
-
-		JPanel p6 = new JPanel();
-		p6.setLayout(new BoxLayout(p6, BoxLayout.PAGE_AXIS));		
-		p6.add(new JLabel("Buscar camino"));
-		JButton getPath = new JButton("Buscar");
-		getEdges.setAlignmentX(LEFT_ALIGNMENT);
-		p6.add(getPath);
-		options.add(p6);
-
-		frame.getContentPane().add(options, BorderLayout.EAST);  // modificacion
-
-		ActionListener searchNode = new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String node = tf1.getText();
-				getNodeSet(node);
-			}
-		};
-		getNodeButton.addActionListener(searchNode);
-
-		ActionListener searchNeighbors = new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String node = tf2.getText();
-				getNeighbors(node);
-			}
-		};
-		getNeighbors.addActionListener(searchNeighbors);
-
 		ActionListener addNewNode = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -176,14 +149,38 @@ public class StructureAdapter extends JApplet
 		};
 		addNode.addActionListener(addNewNode);
 
+		//Delete node panel
+		JPanel p4 = new JPanel();
+		p4.setLayout(new BoxLayout(p4, BoxLayout.PAGE_AXIS));		
+		p4.add(new JLabel("Eliminar nodo"));
+		JTextField tf4 = new JTextField(15);
+		tf4.setPreferredSize(new Dimension(getWidth(),20));
+		p4.add(tf4);
+		JButton delNode = new JButton("Eliminar");
+		delNode.setAlignmentX(LEFT_ALIGNMENT);
+		p4.add(delNode);
+		options.add(p4);
+
 		ActionListener deleteNodeAL = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String nodeTag = tf6.getText();
+				String nodeTag = tf4.getText();
 				deleteNode(nodeTag);
 			}
 		};
 		delNode.addActionListener(deleteNodeAL);
+
+		//Search for edges panel
+		JPanel p5 = new JPanel();
+		p5.setLayout(new BoxLayout(p5, BoxLayout.PAGE_AXIS));		
+		p5.add(new JLabel("Buscar arcos"));
+		JTextField tf5 = new JTextField(15);
+		tf5.setPreferredSize(new Dimension(getWidth(),20));
+		p5.add(tf5);
+		JButton getEdges = new JButton("Buscar");
+		getEdges.setAlignmentX(LEFT_ALIGNMENT);
+		p5.add(getEdges);
+		options.add(p5);
 
 		ActionListener searchEdges = new ActionListener() {
 			@Override
@@ -194,6 +191,38 @@ public class StructureAdapter extends JApplet
 		};
 		getEdges.addActionListener(searchEdges);
 
+		//Add edges panel
+		if(structureType == 2) {
+			JPanel p8 = new JPanel();
+			p8.setLayout(new BoxLayout(p8, BoxLayout.PAGE_AXIS));		
+			p8.add(new JLabel("Agregar  arcos"));
+			JTextField tf8 = new JTextField(15);
+			tf8.setPreferredSize(new Dimension(getWidth(),20));
+			p8.add(tf8);
+			JButton addEdge = new JButton("Agregar");
+			addEdge.setAlignmentX(LEFT_ALIGNMENT);
+			p8.add(addEdge);
+			options.add(p8);
+
+			ActionListener addEdgeAction = new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					String edges = tf8.getText();
+					addEdge(edges);
+				}
+			};
+			addEdge.addActionListener(addEdgeAction);
+		}
+		
+		//Search path panel
+		JPanel p6 = new JPanel();
+		p6.setLayout(new BoxLayout(p6, BoxLayout.PAGE_AXIS));		
+		p6.add(new JLabel("Buscar camino"));
+		JButton getPath = new JButton("Buscar");
+		getPath.setAlignmentX(LEFT_ALIGNMENT);
+		p6.add(getPath);
+		options.add(p6);
+
 		ActionListener drawPath = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -202,6 +231,30 @@ public class StructureAdapter extends JApplet
 		};
 		getPath.addActionListener(drawPath);
 
+		//Show edge set panel
+		if(structureType == 2) {
+			JPanel p7 = new JPanel();
+			p7.setLayout(new BoxLayout(p7, BoxLayout.PAGE_AXIS));
+			p7.add(new JLabel ("Mostrar arcos"));
+			JButton showEdges = new JButton("Mostrar");
+			showEdges.setAlignmentX(LEFT_ALIGNMENT);
+			p7.add(showEdges);
+			options.add(p7);
+
+			ActionListener showEdgesSet = new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					showEdges();
+				}
+			};
+			showEdges.addActionListener(showEdgesSet);
+		}
+
+		//Information panel
+		infoPanel = new JPanel();
+
+		frame.getContentPane().add(infoPanel, BorderLayout.SOUTH);
+		frame.getContentPane().add(options, BorderLayout.EAST);  // modificacion
 		frame.setTitle("Test Estructuras de Datos");  // modificacion
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // modificacion
 		frame.pack();  // modificacion
@@ -532,6 +585,7 @@ public class StructureAdapter extends JApplet
 		defaultColors();
 
 		String message = "";
+		String path = "";
 		ArrayList<Edge<Nodo>> edgesInPath = new ArrayList<Edge<Nodo>>();
 		ArrayList nodeList = sm.getPath();
 		if(nodeList.size() > 1)
@@ -543,6 +597,9 @@ public class StructureAdapter extends JApplet
 					message += nodeList.get(i).toString() + " => " + nodeList.get(i + 1).toString() + ", ";
 				}
 			}
+		for(Object current : nodeList)
+			path += current.toString() + " => ";
+		
 		ArrayList<String> colorNodes = new ArrayList<String>();
 		for(Object node : nodeList){
 			colorNodes.add(node.toString());
@@ -550,11 +607,64 @@ public class StructureAdapter extends JApplet
 
 		switchNodeColor(colorNodes, HIGHLIGHT_COLOR);
 		switchEdgeColor(edgesInPath, HIGHLIGHT_COLOR);
+		System.out.println(path.length());
+		if(path.length() > 0) path = path.substring(0,path.length() - 3);
+		path = "El camino retornado es: " + path;
+		infoPanel.add(new JLabel(path));
 
 		if(!message.isEmpty()){
 			String error = "Los arcos " + message + " no completan el camino.";
 			JOptionPane.showMessageDialog(frame, error, "Error", JOptionPane.ERROR_MESSAGE); 
 		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public void addEdge(String toAdd) {
+		defaultColors();
+		String notAdded = "";
+		String[] edgesSet = toAdd.toString().split(",");
+		ArrayList<Edge> edgesList = new ArrayList<Edge>();
+		ArrayList<String> nodesList = new ArrayList<String>();
+
+		for(String a : edgesSet) {
+			String[] currentEdge = a.split(">");
+			sm.addEdge(currentEdge[0], currentEdge[1]);
+			if(sm.findEdge(currentEdge[0], currentEdge[1]) != null) {
+				Edge found = (Edge) sm.findEdge(currentEdge[0], currentEdge[1]);
+				edgesList.add(found);
+			}
+			if(findNode(currentEdge[0]) != null) nodesList.add(findNode(currentEdge[0]).name);
+			if(findNode(currentEdge[1]) != null) nodesList.add(findNode(currentEdge[1]).name);
+		}
+		ArrayList<Edge<Nodo>> edgesToColor = new ArrayList<Edge<Nodo>>();
+		removeEdgesAndNodes();
+		nodos = createNodes();
+		edges = (structureType == 1) ? createEdgesForList() : createEdges();
+		addEdgesAndNodes();
+		for(Edge current : edgesList) {
+			if(findEdge(current.start.toString(), current.end.toString()) != null)
+				edgesToColor.add(findEdge(current.start.toString(), current.end.toString()));
+			else notAdded += current.start.toString() + ">" + current.end.toString();
+		}
+		if(sm.isLineal() == null || !sm.isLineal()) {
+			mxFastOrganicLayout layout = new  mxFastOrganicLayout(jgxAdapter);
+			layout.execute(jgxAdapter.getDefaultParent());
+		}
+		else {
+			mxHierarchicalLayout layout = new mxHierarchicalLayout(jgxAdapter);
+			layout.execute(jgxAdapter.getDefaultParent());
+		}
+
+		switchNodeColor(nodesList, HIGHLIGHT_COLOR);
+		switchEdgeColor(edgesToColor, HIGHLIGHT_COLOR);
+		if(!notAdded.isEmpty()){
+			String error = "Los arcos " + notAdded + " no se agregaron.";
+			JOptionPane.showMessageDialog(frame, error, "Error", JOptionPane.ERROR_MESSAGE); 
+		}
+	}
+
+	public void showEdges() {
+
 	}
 
 
